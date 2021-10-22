b225c6ccb4fc3993329f3b84295b6e3f8e4de52a
Pfizz <pfiste80@gmail.com>
2021-10-22 02:35:48 -0400

    update thought controller

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -48,6 +48,18 @@ const thoughtController = {
     },
 
     // updateThought({ })
+    updateThought({ params}, res) {
+        Thought.findOneAndUpdate({ _id: params.thoughtId })
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+              res.status(404).json({ message: 'no thought found with this id!' });
+              return;
+            }
+            res.json(dbThoughtData);
+          })
+          .catch(err => res.status(400).json(err));
+    },
+
 
     deleteThought({ params }, res) {
         Thought.findOneAndDelete({ _id: params.thoughtId })
